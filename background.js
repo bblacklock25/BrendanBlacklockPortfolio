1c8bcd29743373787ce100204fedb093fcf5f7dc
bblacklock <bblacklock25@gmail.com>
2021-12-15 23:11:28 -0600

    Updates

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ import { FlakesTexture } from 'FlakesTexture.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({antialias: true});
-
+var c;
 // Initialize necessary variables.
 var cancelAnimation;
 var cancelReverseAnimation;
