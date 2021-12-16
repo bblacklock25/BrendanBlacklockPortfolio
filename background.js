048271ecb5ffae5f73de45c72eaf69f52fa229e6
bblacklock <bblacklock25@gmail.com>
2021-12-16 13:08:20 -0600

    backgroundColor

--- background.js
+++ background.js
@@ -4,6 +4,7 @@ import { FlakesTexture } from '/BrendanBlacklockPortfolio/FlakesTexture.js';
 
 // Define constants needed from three.js to create scene.
 const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xFFFFFF );
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 var c;
