929db2140f49c706c952f47485ff5825cb604dff
bblacklock <bblacklock25@gmail.com>
2021-12-17 00:14:15 -0600

    updatedCursor

--- mainPageTyping.js
+++ mainPageTyping.js
@@ -12,7 +12,7 @@ var counter = 0;
 // Initialize variables for selection cursor.
 let mouseCursor = document.querySelector('.selection-cursor');
 let header = document.querySelector("#name");
-let navbar = document.querySelector('#main-section-nav-tags a');
+let navbar = document.querySelector('#main-section-nav-tags');
 let description = document.querySelector('.main-section-header');
 
 // Event Listeners
