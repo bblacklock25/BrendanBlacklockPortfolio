3e332409ba91e1382a8042f97da1f63689e753cd
bblacklock <bblacklock25@gmail.com>
2021-12-19 21:01:56 -0600

    1

--- mainPageTyping.js
+++ mainPageTyping.js
@@ -80,10 +80,23 @@ if(/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.use
     mobileDevice = 0;
 }
 
+var browserName = (function (agent) {
+  switch (true) {
+      case agent.indexOf("edge") > -1: return "MS Edge";
+      case agent.indexOf("edg/") > -1: return "Edge ( chromium based)";
+      case agent.indexOf("opr") > -1 && !!window.opr: return "Opera";
+      case agent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
+      case agent.indexOf("trident") > -1: return "MS IE";
+      case agent.indexOf("firefox") > -1: return "Mozilla Firefox";
+      case agent.indexOf("safari") > -1: return "Safari";
+      default: return "other";
+  }
+})(window.navigator.userAgent.toLowerCase());
+
 if (mobileDevice == 1) {
     document.getElementById('mobileCursor').remove();
     console.log(0);
-    if (window.navigator.userAgent.toLowerCase().indexOf("chrome") > -1 || window.navigator.userAgent.toLowerCase().indexOf("safari") > -1) {
+    if (browserName == "Chrome" || browserName == "Safari") {
       console.log(1);
       document.getElementById("main-section-nav-tags").classList.toggle("mobile-chrome-safari");
     }
