189e0b6510ec5ddf258dfd551ab9a8a1a3702e46
bblacklock <bblacklock25@gmail.com>
2021-12-23 09:58:36 -0600

    updatesMobile

--- mobile.js
+++ mobile.js
@@ -8,27 +8,26 @@ if(/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.use
 
 var browserName = (function (agent) {
   switch (true) {
-      case agent.indexOf("edga/") > -1: return "MS Edge";
-      case agent.indexOf("edgios/") > -1: return "Edge ( chromium based)";
-      case agent.indexOf("opr") > -1 && !!window.opr: return "Opera";
-      case agent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
-      case agent.indexOf("trident") > -1: return "MS IE";
-      case agent.indexOf("firefox") > -1: return "Mozilla Firefox";
-      case agent.indexOf("safari") > -1: return "Safari";
-      default: return "other";
+    case agent.indexOf("edga/") > -1: return "MS Edge";
+    case agent.indexOf("edgios/") > -1: return "Edge ( chromium based)";
+    case agent.indexOf("opr") > -1 && !!window.opr: return "Opera";
+    case agent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
+    case agent.indexOf("trident") > -1: return "MS IE";
+    case agent.indexOf("firefox") > -1: return "Mozilla Firefox";
+    case agent.indexOf("safari") > -1: return "Safari";
+    default: return "other";
   }
 })(window.navigator.userAgent.toLowerCase());
 
 if (mobileDevice == 1) {
   console.log(0);
-    document.getElementById('mobileCursor').remove();
-    if (browserName == "Chrome" || browserName == "Safari") {
-      console.log(1);
-      document.getElementById("main-section-one").classList.add("mobile-chrome-safari-top");
-      document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-bottom");
-    }
-    if (browserName == "MS Edge" || browserName == "Edge ( chromium based)") {
-      document.getElementById("main-section-one").classList.remove("mobile-chrome-safari-top");
-      document.getElementById("main-section-nav").classList.remove("mobile-chrome-safari-bottom");
-    }
+  if (browserName == "Chrome" || browserName == "Safari") {
+    console.log(1);
+    document.getElementById("main-section-one").classList.add("mobile-chrome-safari-top");
+    document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-bottom");
+  }
+  if (browserName == "MS Edge" || browserName == "Edge ( chromium based)") {
+    document.getElementById("main-section-one").classList.remove("mobile-chrome-safari-top");
+    document.getElementById("main-section-nav").classList.remove("mobile-chrome-safari-bottom");
+  }
 }
