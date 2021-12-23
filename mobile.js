28fcc00d11152d7ee38b1fa91b4b305d9702effa
bblacklock <bblacklock25@gmail.com>
2021-12-23 10:20:36 -0600

    updatesMobile

--- mobile.js
+++ mobile.js
@@ -19,6 +19,8 @@ var browserName = (function (agent) {
   }
 })(window.navigator.userAgent.toLowerCase());
 
+const aboutSectionTwoSubheader = document.querySelector('#about-section-two-text .section-subheader');
+
 if (mobileDevice == 1) {
   console.log(0);
   if (browserName == "Chrome" || browserName == "Safari") {
@@ -27,11 +29,13 @@ if (mobileDevice == 1) {
     document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-main-nav");
     document.getElementById("about-section-one").classList.add("mobile-chrome-safari-about-one");
     document.getElementById("about-section-two").classList.add("mobile-chrome-safari-about-two");
+    aboutSectionTwoSubheader.classList.add('mobile-chrome-safari-about-two-section-subheader');
   }
   if (browserName == "MS Edge" || browserName == "Edge ( chromium based)") {
     document.getElementById("main-section-one").classList.remove("mobile-chrome-safari-main-one");
     document.getElementById("main-section-nav").classList.remove("mobile-chrome-safari-main-nav");
     document.getElementById("about-section-one").classList.remove("mobile-chrome-safari-about-one");
     document.getElementById("about-section-two").classList.remove("mobile-chrome-safari-about-two");
+    aboutSectionTwoSubheader.classList.remove('mobile-chrome-safari-about-two-section-subheader');
   }
 }
