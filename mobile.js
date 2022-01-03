fbf61e2851ad32c1d6584e882af9ed7b656cbe3f
bblacklock <bblacklock25@gmail.com>
2022-01-03 13:51:26 -0600

    safariFontSize

--- mobile.js
+++ mobile.js
@@ -27,6 +27,7 @@ if (mobileDevice == 1) {
     document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-main-nav");
     document.getElementById("about-section-one").classList.add("mobile-safari-about-one");
     document.getElementById("about-section-two").classList.add("mobile-safari-about-two");
+    document.querySelector("#about-section-one-text .section-subheader").classList.add("mobile-safari-about-one-subheader");
     aboutSectionTwoSubheader.classList.add('mobile-chrome-safari-about-two-section-subheader');
   }
   if (navigator.userAgent.match('CriOS')) {
@@ -45,6 +46,7 @@ if (mobileDevice == 1) {
     document.getElementById("about-section-one").classList.remove("mobile-safari-about-one");
     document.getElementById("about-section-two").classList.remove("mobile-safari-about-two");
     document.querySelector("#about-section-one-text .section-subheader").classList.remove("mobile-chrome-about-one-subheader");
+    document.querySelector("#about-section-one-text .section-subheader").classList.remove("mobile-safari-about-one-subheader");
     aboutSectionTwoSubheader.classList.remove('mobile-chrome-safari-about-two-section-subheader');
   }
 }
