e3315c6be0b8e16a73162c034a6908a37016a422
bblacklock <bblacklock25@gmail.com>
2022-01-03 09:42:11 -0600

    mobileSafariChrome

--- mobile.js
+++ mobile.js
@@ -22,18 +22,17 @@ var browserName = (function (agent) {
 const aboutSectionTwoSubheader = document.querySelector('#about-section-two-text .section-subheader');
 
 if (mobileDevice == 1) {
-  console.log(0);
   if (browserName == "Chrome") {
     document.getElementById("main-section-one").classList.add("mobile-chrome-safari-main-one");
     document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-main-nav");
-    document.getElementById("about-section-one").classList.add("mobile-chrome-safari-about-one");
+    document.getElementById("about-section-one").classList.add("mobile-chrome-about-one");
     document.getElementById("about-section-two").classList.add("mobile-chrome-about-two");
     aboutSectionTwoSubheader.classList.add('mobile-chrome-safari-about-two-section-subheader');
   }
   else if (browserName == "Safari") {
     document.getElementById("main-section-one").classList.add("mobile-chrome-safari-main-one");
     document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-main-nav");
-    document.getElementById("about-section-one").classList.add("mobile-chrome-safari-about-one");
+    document.getElementById("about-section-one").classList.add("mobile-safari-about-one");
     document.getElementById("about-section-two").classList.add("mobile-safari-about-two");
     aboutSectionTwoSubheader.classList.add('mobile-chrome-safari-about-two-section-subheader');
   }
