0b7ef1a2bcf25814bd9d5ae38cecd896fcf603ed
bblacklock <bblacklock25@gmail.com>
2022-01-03 09:47:16 -0600

    mobile

--- mobile.js
+++ mobile.js
@@ -39,8 +39,10 @@ if (mobileDevice == 1) {
   if (browserName == "MS Edge" || browserName == "Edge ( chromium based)") {
     document.getElementById("main-section-one").classList.remove("mobile-chrome-safari-main-one");
     document.getElementById("main-section-nav").classList.remove("mobile-chrome-safari-main-nav");
-    document.getElementById("about-section-one").classList.remove("mobile-chrome-safari-about-one");
-    document.getElementById("about-section-two").classList.remove("mobile-chrome-safari-about-two");
+    document.getElementById("about-section-one").classList.remove("mobile-chrome-about-one");
+    document.getElementById("about-section-two").classList.remove("mobile-chrome-about-two");
+    document.getElementById("about-section-one").classList.remove("mobile-safari-about-one");
+    document.getElementById("about-section-two").classList.remove("mobile-safari-about-two");
     aboutSectionTwoSubheader.classList.remove('mobile-chrome-safari-about-two-section-subheader');
   }
 }
