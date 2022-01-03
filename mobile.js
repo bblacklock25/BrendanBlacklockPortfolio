1550b047954baa6dc7b90777e26e96b85a3689a6
bblacklock <bblacklock25@gmail.com>
2022-01-03 09:51:20 -0600

    mobileUpdates

--- mobile.js
+++ mobile.js
@@ -22,18 +22,18 @@ var browserName = (function (agent) {
 const aboutSectionTwoSubheader = document.querySelector('#about-section-two-text .section-subheader');
 
 if (mobileDevice == 1) {
+  if (browserName == "Safari") {
+    document.getElementById("main-section-one").classList.add("mobile-chrome-safari-main-one");
+    document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-main-nav");
+    document.getElementById("about-section-one").classList.add("mobile-safari-about-one");
+    document.getElementById("about-section-two").classList.add("mobile-safari-about-two");
+    aboutSectionTwoSubheader.classList.add('mobile-chrome-safari-about-two-section-subheader');
+  }
   if (browserName == "Chrome") {
     document.getElementById("main-section-one").classList.add("mobile-chrome-safari-main-one");
     document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-main-nav");
     document.getElementById("about-section-one").classList.add("mobile-chrome-about-one");
     document.getElementById("about-section-two").classList.add("mobile-chrome-about-two");
-    aboutSectionTwoSubheader.classList.add('mobile-chrome-safari-about-two-section-subheader');
-  }
-  else if (browserName == "Safari") {
-    document.getElementById("main-section-one").classList.add("mobile-chrome-safari-main-one");
-    document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-main-nav");
-    document.getElementById("about-section-one").classList.add("mobile-safari-about-one");
-    document.getElementById("about-section-two").classList.add("mobile-safari-about-two");
     aboutSectionTwoSubheader.classList.add('mobile-chrome-safari-about-two-section-subheader');
   }
   if (browserName == "MS Edge" || browserName == "Edge ( chromium based)") {
