97b91c55303c977cb1e4f4b9a66454244c0141ef
bblacklock <bblacklock25@gmail.com>
2022-01-03 13:31:01 -0600

    edgeChromeChanges

--- mobile.js
+++ mobile.js
@@ -34,6 +34,7 @@ if (mobileDevice == 1) {
     document.getElementById("main-section-nav").classList.add("mobile-chrome-safari-main-nav");
     document.getElementById("about-section-one").classList.add("mobile-chrome-about-one");
     document.getElementById("about-section-two").classList.add("mobile-chrome-about-two");
+    document.querySelector("#about-section-one-text .section-subheader").classList.add("mobile-chrome-about-one-subheader");
     aboutSectionTwoSubheader.classList.add('mobile-chrome-safari-about-two-section-subheader');
   }
   if (browserName == "MS Edge" || browserName == "Edge ( chromium based)") {
@@ -43,6 +44,7 @@ if (mobileDevice == 1) {
     document.getElementById("about-section-two").classList.remove("mobile-chrome-about-two");
     document.getElementById("about-section-one").classList.remove("mobile-safari-about-one");
     document.getElementById("about-section-two").classList.remove("mobile-safari-about-two");
+    document.querySelector("#about-section-one-text .section-subheader").classList.remove("mobile-chrome-about-one-subheader");
     aboutSectionTwoSubheader.classList.remove('mobile-chrome-safari-about-two-section-subheader');
   }
 }
