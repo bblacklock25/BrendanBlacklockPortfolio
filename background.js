2ad8cd58ffca72c6bc35ec29711cbded69de74a2
bblacklock <bblacklock25@gmail.com>
2022-01-06 14:56:08 -0600

    pathingUpdates

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 // Import libraries
-import * as THREE from '/three.module.js';
-import { FlakesTexture } from '/FlakesTexture.js';
+import * as THREE from '/BrendanBlacklockPortfolio/three.module.js';
+import { FlakesTexture } from '/BrendanBlacklockPortfolio/FlakesTexture.js';
 
 // Define constants needed from three.js to create scene.
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x101010);
+scene.background = new THREE.Color(0x101010);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 // Initialize necessary variables.
